bd5ab252ce251a89919335ab1197c6d019cdf2b8
evsmax <40871583+evsmax@users.noreply.github.com>
2018-10-13 23:24:13 +0400

    Create page_52.cpp

---
+++ practice/page_52.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "input n:= "; cin >> n;
+  double b = 0.1;
+  for (int i = 2; i <= n; i++)
+  {
+    b = (1 % 6) * (0.05 = pow( b, 3));
+    cout << "b" << i << "= " << b << endl;
+  }
+  return 0;
+}
